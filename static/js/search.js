2b3bd8aeeb1a66131cad259feb2e930c11cde681
kimkimani <kimkimani016@gmail.com>
2023-08-22 16:20:59 +0300

    update

--- static/js/search.js
+++ static/js/search.js
@@ -1,135 +1,141 @@
 var summaryInclude = 120;
 var fuseOptions = {
-    shouldSort: true,
-    includeMatches: true,
-    threshold: 0.0,
-    tokenize: true,
-    location: 0,
-    distance: 100,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: [
-        {name: "title", weight: 0.8},
-        {name: "contents", weight: 0.5},
-        {name: "topics", weight: 0.3},
-      
-    ]
+  shouldSort: true,
+  includeMatches: true,
+  threshold: 0.0,
+  tokenize: true,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    { name: "title", weight: 0.8 },
+    { name: "contents", weight: 0.5 },
+    { name: "topics", weight: 0.3 },
+  ],
 };
 
 var show = function (elem) {
-    elem.style.display = 'block';
+  elem.style.display = "block";
 };
 var hide = function (elem) {
-    elem.style.display = 'none';
+  elem.style.display = "none";
 };
 
-var inputBox = document.getElementById('search-query');
+var inputBox = document.getElementById("search-query");
 if (inputBox !== null) {
-    var searchQuery = param("q");
-    if (searchQuery) {
-        inputBox.value = searchQuery || "";
-        executeSearch(searchQuery, false);
-    } else {
-        document.getElementById('search-results').innerHTML = '<p class="search-results-empty"></p>';
-    }
+  var searchQuery = param("q");
+  if (searchQuery) {
+    inputBox.value = searchQuery || "";
+    executeSearch(searchQuery, false);
+  } else {
+    document.getElementById("search-results").innerHTML =
+      '<p class="search-results-empty"></p>';
+  }
 }
 
 function executeSearch(searchQuery) {
+  show(document.querySelector(".search-loading"));
 
-    show(document.querySelector('.search-loading'));
-
-    fetch('/index.json').then(function (response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' + response.status);
-            return;
+  fetch("/index.json").then(function (response) {
+    if (response.status !== 200) {
+      console.log(
+        "Looks like there was a problem. Status Code: " + response.status
+      );
+      return;
+    }
+    // Examine the text in the response
+    response
+      .json()
+      .then(function (pages) {
+        var fuse = new Fuse(pages, fuseOptions);
+        var result = fuse.search(searchQuery);
+        if (result.length > 0) {
+          populateResults(result);
+        } else {
+          document.getElementById("search-results").innerHTML =
+            '<p class="search-results-empty">No matches found</p>';
         }
-        // Examine the text in the response
-        response.json().then(function (pages) {
-            var fuse = new Fuse(pages, fuseOptions);
-            var result = fuse.search(searchQuery);
-            if (result.length > 0) {
-                populateResults(result);
-            } else {
-                document.getElementById('search-results').innerHTML = '<p class=\"search-results-empty\">No matches found</p>';
-            }
-            hide(document.querySelector('.search-loading'));
-        })
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-    });
+        hide(document.querySelector(".search-loading"));
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  });
 }
 
 function populateResults(results) {
+  var searchQuery = document.getElementById("search-query").value;
+  var searchResults = document.getElementById("search-results");
 
-    var searchQuery = document.getElementById("search-query").value;
-    var searchResults = document.getElementById("search-results");
+  // pull template from hugo template definition
+  var templateDefinition = document.getElementById("search-result").innerHTML;
 
-    // pull template from hugo template definition
-    var templateDefinition = document.getElementById("search-result").innerHTML;
+  results.forEach(function (value, key) {
+    var contents = value.item.contents;
+    var snippet = "";
+    var snippetHighlights = [];
 
-    results.forEach(function (value, key) {
+    snippetHighlights.push(searchQuery);
+    snippet = contents.substring(0, summaryInclude * 2) + "&hellip;";
 
-        var contents = value.item.contents;
-        var snippet = "";
-        var snippetHighlights = [];
+    //replace values
+    var topics = "";
+    if (value.item.topics) {
+      value.item.topics.forEach(function (element) {
+        topics =
+          topics + "<a href='/topics/" + element + "'>" + element + "</a>, ";
+      });
+    }
+    if (topics.length > 2) {
+      topics = topics.substring(0, topics.length - 2);
+    }
 
-        snippetHighlights.push(searchQuery);
-        snippet = contents.substring(0, summaryInclude * 2) + '&hellip;';
-
-        //replace values
-        var topics = ""
-        if (value.item.topics) {
-            value.item.topics.forEach(function (element) {
-                topics = topics + "<a href='/topics/" + element + "'>" + element + "</a>, "
-            });
-        }
-        if (topics.length > 2) {
-            topics = topics.substring(0,topics.length-2);
-        }
-
-        var output = render(templateDefinition, {
-            key: key,
-            title: value.item.title,
-            link: value.item.permalink,
-            topics: topics,
-            snippet: snippet
-        });
-        searchResults.innerHTML += output;
-
-        snippetHighlights.forEach(function (snipvalue, snipkey) {
-            var instance = new Mark(document.getElementById('summary-' + key));
-            instance.mark(snipvalue);
-        });
-
+    var output = render(templateDefinition, {
+      key: key,
+      title: value.item.title,
+      link: value.item.permalink,
+      topics: topics,
+      snippet: snippet,
     });
+    searchResults.innerHTML += output;
+
+    snippetHighlights.forEach(function (snipvalue, snipkey) {
+      var instance = new Mark(document.getElementById("summary-" + key));
+      instance.mark(snipvalue);
+    });
+  });
 }
 
 function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
+  return decodeURIComponent(
+    (location.search.split(name + "=")[1] || "").split("&")[0]
+  ).replace(/\+/g, " ");
 }
 
 function render(templateString, data) {
-    var conditionalMatches, conditionalPattern, copy;
-    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-    copy = templateString;
-    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-        if (data[conditionalMatches[1]]) {
-            //valid key, remove conditionals, leave contents.
-            copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
-        } else {
-            //not valid, remove entire section
-            copy = copy.replace(conditionalMatches[0], '');
-        }
+  var conditionalMatches, conditionalPattern, copy;
+  conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+  //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+  copy = templateString;
+  while (
+    (conditionalMatches = conditionalPattern.exec(templateString)) !== null
+  ) {
+    if (data[conditionalMatches[1]]) {
+      //valid key, remove conditionals, leave contents.
+      copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
+    } else {
+      //not valid, remove entire section
+      copy = copy.replace(conditionalMatches[0], "");
     }
-    templateString = copy;
-    //now any conditionals removed we can do simple substitution
-    var key, find, re;
-    for (key in data) {
-        find = '\\$\\{\\s*' + key + '\\s*\\}';
-        re = new RegExp(find, 'g');
-        templateString = templateString.replace(re, data[key]);
-    }
-    return templateString;
+  }
+  templateString = copy;
+  //now any conditionals removed we can do simple substitution
+  var key, find, re;
+  for (key in data) {
+    find = "\\$\\{\\s*" + key + "\\s*\\}";
+    re = new RegExp(find, "g");
+    templateString = templateString.replace(re, data[key]);
+  }
+  return templateString;
 }
